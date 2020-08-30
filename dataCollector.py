ecdb06c8d0f519c123b6e329c39c0014eac49306
unknown <evan.a.west@gmail.com>
2020-08-30 00:46:50 -0400

    Add DropBox upload

--- dataCollector.py
+++ dataCollector.py
@@ -6,12 +6,29 @@ import time
 import os
 import datetime
 from pytz import timezone
-r.login("evan-west@hotmail.com", "Ambrosio%67")
+import sys
+import dropbox
+from dropbox.files import WriteMode
+from dropbox.exceptions import ApiError, AuthError
 
-DEBUGGER = 1
+
+# Setup Robinhood account
+r.login("evan-west@hotmail.com", "Ambrosio%67")
+# Access token to Evan's Dropbox
+# TOKEN = 'sl.AgvJduh0Hme3yN02cpSv7Omx-upyHV8Uwvq_7OM5mVHr_qYH-mpw_IABmFkuZwCtTQQEFCS7XpXulA6Y9fZIYQFz-wA11kj_fgrnPHCsIlXXSfWhe9QyLf-DIgdybMEnyDHUUUE'
+TOKEN = 'CuaXlDyGECgAAAAAAAAAAZYAeOmfjT2c2UcrE7CcWNLU2dz5hRbLaNQGrxx2GK4H'
+# Root path at which to save files. Keep the forward slash before destination filename
+BACKUPPATH_ROOT = '/Server1'
+# Set DEBUGGER to 1 to print debugging statements
+DEBUGGER = 0
 
 
 def setup():
+    # Check for an access token
+    if (len(TOKEN) == 0):
+        sys.exit("ERROR: Looks like you didn't add your access token. Open up backup-and-restore-example.py in a text editor and paste in your token in line 14.")
+     # Create an instance of a Dropbox class, which can make requests to the API.
+
     if(DEBUGGER == 1):
         print("inside setup")
 
@@ -22,7 +39,19 @@ def setup():
     return codes
 
 
-def save_data(code, data, current_datetime, file_upload_time):
+def save_data(code, data, current_datetime, file_upload_time, previous_filename, backup_proc):
+    if(not (backup_proc is None)):
+        if(DEBUGGER == 1):
+            print(
+                "\n\n###########################################################################")
+            print("backup_proc: " + str(backup_proc))
+            print("backup_proc is None: " + str(backup_proc is None) +
+                  ", Note - expecting FALSE here")
+            print("Waiting for previous backup process to complete")
+        backup_proc.join()
+        if(DEBUGGER == 1):
+            print("Previous backup process is complete")
+
     if(DEBUGGER == 1):
         print("Inside save_data")
 
@@ -40,7 +69,7 @@ def save_data(code, data, current_datetime, file_upload_time):
     if(not os.path.exists(directory)):
         os.makedirs(directory)
 
-    file_append = current_datetime.strftime("%d_%m_%Y_Hour%H")
+    file_append = current_datetime.strftime("%d_%m_%Y_Hour%H_Minute%M")
 
     filename = directory + '/' + code + '_' + file_append + '.csv'
 
@@ -61,8 +90,29 @@ def save_data(code, data, current_datetime, file_upload_time):
         with open(filename, 'a') as outfile:
             np.savetxt(fname=outfile, X=numpy_data, delimiter=',', fmt='%s')
 
-    # Upload the file to dropbox every three hours
-    # ...Need logic here
+    # Upload the file to dropbox if the file_upload_time has been reached or if the filename changed.
+    # If the filename changed, then the final version of the previous file can be changed
+    # Note: This will overwrite any existing file on Dropbox
+    if(DEBUGGER == 1):
+        print("(current_datetime - file_upload_time).total_seconds() : " +
+              str((current_datetime - file_upload_time).total_seconds()))
+        print("(current_datetime - file_upload_time).total_seconds() >= 0 : " +
+              str((current_datetime - file_upload_time).total_seconds() >= 0))
+        print("previous_filename : " + previous_filename)
+        print("filename : " + filename)
+        print("previous_filename != filename : " +
+              str(previous_filename != filename))
+    backup_proc = None
+    if((current_datetime - file_upload_time).total_seconds() >= 0 or (previous_filename != filename)):
+        # On the first iteration of the program, the previous_filename will be null, and we want to update it to the new filname
+        if(previous_filename == ''):
+            previous_filename = filename
+        filepath = previous_filename
+        backuppath = BACKUPPATH_ROOT + "/" + filepath
+
+        backup_proc = mp.Process(target=backup, args=(backuppath, filepath,))
+        backup_proc.start()
+    return {'filename': filename, 'backup_proc': backup_proc}
 
 
 def process_func(code):
@@ -77,7 +127,8 @@ def process_func(code):
     loop_counter = 0  # Will trigger a while write when loop_counter == write_threshold
     file_upload_time = 0  # For tracking the previous file upload
     time_between_uploads = 3  # Hours between uploads to Dropbox
-
+    previous_filename = ''  # For tracking when filename changes. This is needed so that if the filename changes between upload intervals, the final version of the previous file can be uploaded
+    backup_proc = None  # Allows the backup process generated in save_data to run in parrellel. Most of the time, the process will finish backing up to DropBox before the next call to save_data, but this is needed to handle the situations where the backup has not completed yet
     while (1+1 == 2):  # Loop INFINITELY
         if(DEBUGGER == 1):
             print("Inside process_func while loop with loop_counter = " +
@@ -93,8 +144,9 @@ def process_func(code):
 
         # Update the fileupload time, if an upload occurred in the previous loop
         if(file_upload_time == 0):
-            file_upload_time = datetime.timedelta(hours=time_between_uploads)
-
+            delta = datetime.timedelta(hours=time_between_uploads)
+            # delta = datetime.timedelta(seconds=time_between_uploads)
+            file_upload_time = current_datetime + delta
         # Save the newest record
         data.append(local_dict)
 
@@ -105,8 +157,10 @@ def process_func(code):
         if(loop_counter >= write_threshold):
 
             # Save the accumulated data
-            save_data(code, data, current_datetime, file_upload_time)
-
+            info_from_save_data = save_data(
+                code, data, current_datetime, file_upload_time, previous_filename, backup_proc)
+            previous_filename = info_from_save_data.get("filename")
+            backup_proc = info_from_save_data.get("backup_proc")
             # counters and data
             loop_counter = 0
             data = []
@@ -114,13 +168,63 @@ def process_func(code):
         loop_counter += 1
 
 
+# Uploads contents of LOCALFILE to Dropbox
+def backup(backuppath, filepath):
+    if(DEBUGGER == 1):
+        print('backuppath: ' + backuppath)
+        print('filepath: ' + filepath)
+        print("Creating a Dropbox object...")
+
+    dbx = dropbox.Dropbox(TOKEN)
+
+    # Check that the access token is valid
+    try:
+        dbx.users_get_current_account()
+    except AuthError as err:
+        sys.exit(
+            "ERROR: Invalid access token; try re-generating an access token from the app console on the web.")
+
+    with open(filepath, 'rb') as f:
+        # We use WriteMode=overwrite to make sure that the settings in the file
+        # are changed on upload
+        if(DEBUGGER == 1):
+            print("Uploading " + filepath +
+                  " to Dropbox as " + backuppath + "...")
+
+        try:
+            dbx.files_upload(f.read(), backuppath,
+                             mode=WriteMode('overwrite'))
+        except ApiError as err:
+            # This checks for the specific error where a user doesn't have enough Dropbox space quota to upload this file
+            if (err.error.is_path() and
+                    err.error.get_path().error.is_insufficient_space()):
+                sys.exit("ERROR: Cannot back up; insufficient space.")
+            elif err.user_message_text:
+                print(err.user_message_text)
+                sys.exit()
+            else:
+                print(err)
+                sys.exit()
+
+
+# # Adding few functions to check file details
+# def checkFileDetails():
+#     print("Checking file details")
+
+#     for entry in dbx.files_list_folder('').entries:
+#         print("File list is : ")
+#         print(entry.name)
+
 def main():
+
+    # Setup connection to
     process_names = setup()
 
     if(DEBUGGER == 1):
         print(process_names)
     procs = []
 
+    # process_names = ['ETC']
     for name in process_names:
         proc = mp.Process(target=process_func, args=(name,))
         procs.append(proc)
