095bbf5550b0af419acfffc07d75b7d4fd5fb452
unknown <evan.a.west@gmail.com>
2020-09-17 19:03:18 -0400

    Initial rework, untested

--- dataCollector.py
+++ dataCollector.py
@@ -24,9 +24,9 @@ except ConnectionError as err:
 # TOKEN = 'sl.AgvJduh0Hme3yN02cpSv7Omx-upyHV8Uwvq_7OM5mVHr_qYH-mpw_IABmFkuZwCtTQQEFCS7XpXulA6Y9fZIYQFz-wA11kj_fgrnPHCsIlXXSfWhe9QyLf-DIgdybMEnyDHUUUE'
 TOKEN = 'CuaXlDyGECgAAAAAAAAAAZYAeOmfjT2c2UcrE7CcWNLU2dz5hRbLaNQGrxx2GK4H'
 # Root path at which to save files. Keep the forward slash before destination filename
-BACKUPPATH_ROOT = '/Server1'
+BACKUPPATH_ROOT = '/local'
 # Set DEBUGGER to 1 to print debugging statements
-DEBUGGER = 0
+DEBUGGER = 1
 
 
 def setup():
@@ -53,7 +53,7 @@ def setup():
     return codes
 
 
-def save_data(code, data, current_datetime, file_upload_time, previous_filename, backup_thread):
+def save_data(coinCodes, allData, current_datetime, file_upload_time, previous_filenames, backup_thread):
     if(not (backup_thread is None)):
         if(DEBUGGER == 1):
             print(
@@ -69,40 +69,43 @@ def save_data(code, data, current_datetime, file_upload_time, previous_filename,
     if(DEBUGGER == 1):
         print("Inside save_data")
 
-    # Extract raw data form the dictionary
-    raw_data = []
-    for record in data:
-        row = []
-        for item in record:
-            row.append(record.get(item))
-        raw_data.append(row)
-    numpy_data = np.array(raw_data)
+    filenames = {}
+    # Extract raw data from the dictionary
+    for code in coinCodes:
+        raw_data = []
+        for record in allData.get(code):
+            row = []
+            for item in record:
+                row.append(record.get(item))
+            raw_data.append(row)
+        numpy_data = np.array(raw_data)
 
-    # Create directories, as needed
-    directory = 'Data/' + code
-    if(not os.path.exists(directory)):
-        os.makedirs(directory)
+        # Create directories, as needed
+        directory = 'Data/' + code
+        if(not os.path.exists(directory)):
+            os.makedirs(directory)
 
-    file_append = current_datetime.strftime("%d_%m_%Y")
+        file_append = current_datetime.strftime("%d_%m_%Y")
 
-    filename = directory + '/' + code + '_' + file_append + '.csv'
+        filename = directory + '/' + code + '_' + file_append + '.csv'
+        filenames.update(code, filename)
+        # If the file does not exist, create it and output the headers
+        if(not os.path.exists(filename)):
+            # Generate headers
+            headers = ''
+            for i in data[0].keys():
+                headers += i + ','
+            # Create the new file and save data with headers
+            with open(filename, 'a') as outfile:
+                np.savetxt(fname=outfile, X=numpy_data,
+                           delimiter=',', fmt='%s', header=str(headers))
 
-    # If the file does not exist, create it and output the headers
-    if(not os.path.exists(filename)):
-        # Generate headers
-        headers = ''
-        for i in data[0].keys():
-            headers += i + ','
-        # Create the new file and save data with headers
-        with open(filename, 'a') as outfile:
-            np.savetxt(fname=outfile, X=numpy_data,
-                       delimiter=',', fmt='%s', header=str(headers))
-
-    # If the file exists, save data without headers
-    else:
-        # Append data to the file
-        with open(filename, 'a') as outfile:
-            np.savetxt(fname=outfile, X=numpy_data, delimiter=',', fmt='%s')
+        # If the file exists, save data without headers
+        else:
+            # Append data to the file
+            with open(filename, 'a') as outfile:
+                np.savetxt(fname=outfile, X=numpy_data,
+                           delimiter=',', fmt='%s')
 
     # Upload the file to dropbox if the file_upload_time has been reached or if the filename changed.
     # If the filename changed, then the final version of the previous file can be uploaded
@@ -112,165 +115,164 @@ def save_data(code, data, current_datetime, file_upload_time, previous_filename,
               str((current_datetime - file_upload_time).total_seconds()))
         print("(current_datetime - file_upload_time).total_seconds() >= 0 : " +
               str((current_datetime - file_upload_time).total_seconds() >= 0))
-        print("previous_filename : " + previous_filename)
-        print("filename : " + filename)
-        print("previous_filename != filename : " +
-              str(previous_filename != filename))
+        print("previous_filenames : " + previous_filenames)
+        print("filenames : " + filenames)
+        print("previous_filenames != filenames : " +
+              str(previous_filenames != filenames))
     backup_thread = None
-    if((current_datetime - file_upload_time).total_seconds() >= 0 or (previous_filename != filename)):
+    if((current_datetime - file_upload_time).total_seconds() >= 0 or (previous_filenames != filenames)):
+        backup_thread = thr.Thread(
+            target=backup, args=(previous_filenames, filenames))
+        backup_thread.start()
+    return {'filename': filename, 'backup_thread': backup_thread}
+
+
+def process_func(code):
+    if(DEBUGGER == 1):
+        print("Starting process_func for process: " + str(mp.current_process()))
+
+    if(DEBUGGER == 1):
+        print("stop_threads : " + str(stop_threads()) +
+              ", Note: expecting FALSE until user input")ls
+        print("not stop_threads : " + str(not stop_threads()) +
+              ", Note: expecting TRUE until user input")
+
+    if(DEBUGGER == 1):
+        print("Inside process_func while loop with loop_counter = " +
+              str(loop_counter))
+
+    try:
+        # Get current quote from RobinHood
+        local_dict = r.crypto.get_crypto_quote(code, info=None)
+    except ConnectionError as err:
+        logging.error(
+            "Connection Error while retrieving a quote for " + code)
+        logging.error(err)
+
+    # Append the datetime to the record
+    est = timezone('EST')
+    current_datetime = datetime.datetime.now(est)
+    local_dict.update({'datetime': str(current_datetime)})
+
+    return local_dict
+
+
+# Uploads contents of LOCALFILE to Dropbox
+def backup(previous_filenames, filenames):
+    for previous_filename, filename in zip(previous_filenames, filenames):
         # On the first iteration of the program, the previous_filename will be null, and we want to update it to the new filname
         if(previous_filename == code + '_firstupload.csv'):
             previous_filename = filename
         filepath = previous_filename
         backuppath = BACKUPPATH_ROOT + "/" + filepath
 
-        backup_thread = thr.Thread(target=backup, args=(backuppath, filepath,))
-        backup_thread.start()
-    return {'filename': filename, 'backup_thread': backup_thread}
+        if(DEBUGGER == 1):
+            print('backuppath: ' + backuppath)
+            print('filepath: ' + filepath)
+            print("Creating a Dropbox object...")
+
+        dbx = dropbox.Dropbox(TOKEN)
+
+        # Check that the access token is valid
+        try:
+            dbx.users_get_current_account()
+        except AuthError as err:
+            sys.exit(
+                "ERROR: Invalid access token; try re-generating an access token from the app console on the web.")
+
+        with open(filepath, 'rb') as f:
+            # We use WriteMode=overwrite to make sure that the settings in the file
+            # are changed on upload
+            if(DEBUGGER == 1):
+                print("Uploading " + filepath +
+                      " to Dropbox as " + backuppath + "...")
+
+            try:
+                dbx.files_upload(f.read(), backuppath,
+                                 mode=WriteMode('overwrite'))
+            except ApiError as err:
+                # This checks for the specific error where a user doesn't have enough Dropbox space quota to upload this file
+                if (err.error.is_path() and
+                        err.error.get_path().error.is_insufficient_space()):
+                    sys.exit("ERROR: Cannot back up; insufficient space.")
+                elif err.user_message_text:
+                    print(err.user_message_text)
+                    sys.exit()
+                else:
+                    print(err)
+                    sys.exit()
 
 
-def process_func(code, stop_threads):
-    if(DEBUGGER == 1):
-        print("Starting process_func for process: " + str(mp.current_process()))
-    data = []
+def logging(filename):
+    filepath = l
+    with open(filepath, 'rb') as f:
+
+        # # Adding few functions to check file details
+        # def checkFileDetails():
+        #     print("Checking file details")
+
+        #     for entry in dbx.files_list_folder('').entries:
+        #         print("File list is : ")
+        #         print(entry.name)
+
+
+def main():
+    # Setup connection to
+    coinCodes = setup()
     duration = 5.0  # Duration of sleep in seconds
 
     # Threshold for triggering file write. The value here is the number of loops between file writes (i.e. 36 = 3 minutes when duration is set to 5 seconds)
     write_threshold = 36
 
     loop_counter = 0  # Will trigger a while write when loop_counter == write_threshold
+
     file_upload_time = 0  # For tracking the previous file upload
+
     time_between_uploads = 3  # Hours between uploads to Dropbox
-    # For tracking when filename changes. This is needed so that if the filename changes between upload intervals, the final version of the previous file can be uploaded
-    previous_filename = code + '_firstupload.csv'
+
+    # data - For each coin, save all data
+    # previous_filename - For each coin, track when filename changes. This is needed so that if the filename changes between upload intervals, the final version of the previous file can be uploaded
+    allData = {}
+    previous_filenames = {}
+    for code in coinCodes:
+        previous_filenames.update(code, code + '_firstupload.csv')
+        allData.update(code, [])
+
     backup_thread = None  # Allows the backup process generated in save_data to run in parrellel. Most of the time, the process will finish backing up to DropBox before the next call to save_data, but this is needed to handle the situations where the backup has not completed yet
 
     if(DEBUGGER == 1):
-        print("stop_threads : " + str(stop_threads()) +
-              ", Note: expecting FALSE until user input")
-        print("not stop_threads : " + str(not stop_threads()) +
-              ", Note: expecting TRUE until user input")
+        print(process_names)
 
-    while (not stop_threads()):  # Loop INFINITELY until user says to stop
-        if(DEBUGGER == 1):
-            print("Inside process_func while loop with loop_counter = " +
-                  str(loop_counter))
-
-        try:
-            # Get current quote from RobinHood
-            local_dict = r.crypto.get_crypto_quote(code, info=None)
-        except ConnectionError as err:
-            logging.error(
-                "Connection Error while retrieving a quote for " + code)
-            logging.error(err)
-
-        # Append the datetime to the record
-        est = timezone('EST')
-        current_datetime = datetime.datetime.now(est)
-        local_dict.update({'datetime': str(current_datetime)})
+    while true:
+        for code in coinCodes:
+            coindata = allData.get(code)
+            coindata.append(process_func(code))
+            allData.update(code, coindata)
 
         # Update the fileupload time, if an upload occurred in the previous loop
         if(file_upload_time == 0):
             delta = datetime.timedelta(hours=time_between_uploads)
             # delta = datetime.timedelta(seconds=time_between_uploads)
             file_upload_time = current_datetime + delta
-        # Save the newest record
-        data.append(local_dict)
-
-        # Wait
-        time.sleep(duration)
 
         # Save the data to a file based on the write_threshold
         if(loop_counter >= write_threshold):
 
             # Save the accumulated data
             info_from_save_data = save_data(
-                code, data, current_datetime, file_upload_time, previous_filename, backup_thread)
-            previous_filename = info_from_save_data.get("filename")
+                coinCodes, allData, current_datetime, file_upload_time, previous_filenames, backup_thread)
+            previous_filenames = info_from_save_data.get("filename")
             backup_thread = info_from_save_data.get("backup_thread")
-            # counters and data
-            loop_counter = 0
-            data = []
 
+            # reset counter and data
+            loop_counter = 0
+            allData = {}
+            for code in coinCodes:
+                allData.update(code, [])
         loop_counter += 1
 
-
-# Uploads contents of LOCALFILE to Dropbox
-def backup(backuppath, filepath):
-    if(DEBUGGER == 1):
-        print('backuppath: ' + backuppath)
-        print('filepath: ' + filepath)
-        print("Creating a Dropbox object...")
-
-    dbx = dropbox.Dropbox(TOKEN)
-
-    # Check that the access token is valid
-    try:
-        dbx.users_get_current_account()
-    except AuthError as err:
-        sys.exit(
-            "ERROR: Invalid access token; try re-generating an access token from the app console on the web.")
-
-    with open(filepath, 'rb') as f:
-        # We use WriteMode=overwrite to make sure that the settings in the file
-        # are changed on upload
-        if(DEBUGGER == 1):
-            print("Uploading " + filepath +
-                  " to Dropbox as " + backuppath + "...")
-
-        try:
-            dbx.files_upload(f.read(), backuppath,
-                             mode=WriteMode('overwrite'))
-        except ApiError as err:
-            # This checks for the specific error where a user doesn't have enough Dropbox space quota to upload this file
-            if (err.error.is_path() and
-                    err.error.get_path().error.is_insufficient_space()):
-                sys.exit("ERROR: Cannot back up; insufficient space.")
-            elif err.user_message_text:
-                print(err.user_message_text)
-                sys.exit()
-            else:
-                print(err)
-                sys.exit()
-
-
-# # Adding few functions to check file details
-# def checkFileDetails():
-#     print("Checking file details")
-
-#     for entry in dbx.files_list_folder('').entries:
-#         print("File list is : ")
-#         print(entry.name)
-
-def main():
-    # Setup connection to
-    process_names = setup()
-
-    if(DEBUGGER == 1):
-        print(process_names)
-    # procs = []
-    threads = []
-
-    # process_names = ['ETC']
-    stop_threads = False
-    for name in process_names:
-        # proc = mp.Process(target=process_func, args=(name,))
-        # procs.append(proc)
-        # proc.start()
-        thread = thr.Thread(target=process_func,
-                            args=(name, lambda: stop_threads))
-        threads.append(thread)
-        thread.start()
-
-    while not stop_threads:
-        userInput = input("input 'exit' to stop : ")
-        if(userInput == "exit"):
-            stop_threads = True
-
-    # for proc in procs:
-    #     proc.join()
-    for thread in threads:
-        thread.join()
+        # Wait
+        time.sleep(duration)
 
 
 if __name__ == "__main__":
