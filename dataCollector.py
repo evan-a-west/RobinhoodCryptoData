e5663c083e07f1c56d819805088bcbda085e1242
unknown <evan.a.west@gmail.com>
2020-11-08 02:07:42 -0500

    Remove drop-box uploading from the data collector. It now justs writes to local files. include a logging function

--- dataCollector.py
+++ dataCollector.py
@@ -15,42 +15,36 @@ import logging
 from requests.exceptions import ConnectionError
 import json
 import pyotp
+from os.path import isfile, join
+from os import listdir
 
 try:
     # Setup Robinhood account
     totp = pyotp.TOTP("3FYIQZB7NUF2SS6J").now()
     r.login("evan-west@hotmail.com", "Ambrosio%67", mfa_code=totp)
 except ConnectionError as err:
-    logging.error("Connection Error while logging in")
-    logging.error(err)
-# Access token to Evan's Dropbox
-# TOKEN = 'sl.AgvJduh0Hme3yN02cpSv7Omx-upyHV8Uwvq_7OM5mVHr_qYH-mpw_IABmFkuZwCtTQQEFCS7XpXulA6Y9fZIYQFz-wA11kj_fgrnPHCsIlXXSfWhe9QyLf-DIgdybMEnyDHUUUE'
-TOKEN = 'CuaXlDyGECgAAAAAAAAAAZYAeOmfjT2c2UcrE7CcWNLU2dz5hRbLaNQGrxx2GK4H'
-# Root path at which to save files. Keep the forward slash before destination filename
-BACKUPPATH_ROOT = '/local'
+    log("Connection Error while logging in")
+    log(err)
 
-# Set DEBUGGER to 1 to print debugging statements
+# Set DEBUGGER to 1 to log debugging statements
 DEBUGGER = 0
 
 LOCAL_DATA_DIRECTORY = 'Data/'
+LOGGING_DIRECTORY = LOCAL_DATA_DIRECTORY + "Logs/"
+FILE_APPEND = datetime.datetime.now(timezone('EST')).strftime("%d_%m_%Y_%M")
 
 
 def setup():
-    # Check for an access token
-    if (len(TOKEN) == 0):
-        sys.exit("ERROR: Looks like you didn't add your access token. Open up backup-and-restore-example.py in a text editor and paste in your token in line 14.")
-     # Create an instance of a Dropbox class, which can make requests to the API.
-
     if(DEBUGGER == 1):
-        print("inside setup")
+        log("inside setup")
 
     all_currency_info = None
     try:
         # Retrieve all currency information from Robinhood
         all_currency_info = r.crypto.get_crypto_currency_pairs(info=None)
     except ConnectionError as err:
-        logging.error("Connection Error while retreiving currency information")
-        logging.error(err)
+        log("Connection Error while retreiving currency information")
+        log(err)
 
     # Build array of currency codes
     codes = []
@@ -59,11 +53,9 @@ def setup():
     return codes
 
 
-def save_data(coinCodes, allData, current_datetime, file_upload_time, previous_filenames):
-    time_between_uploads = 30  # Minutes between uploads to Dropbox
-
+def save_data(coinCodes, allData, current_datetime):
     if(DEBUGGER == 1):
-        print("Inside save_data")
+        log("Inside save_data")
 
     filenames = {}
     # Extract raw data from the dictionary
@@ -82,10 +74,7 @@ def save_data(coinCodes, allData, current_datetime, file_upload_time, previous_f
         if(not os.path.exists(directory)):
             os.makedirs(directory)
 
-        # file_append = current_datetime.strftime("%d_%m_%Y")
-        file_append = current_datetime.strftime("%d_%m_%Y")
-
-        filename = directory + '/' + code + '_' + file_append + '.csv'
+        filename = directory + '/' + code + '_' + FILE_APPEND + '.csv'
         filenames.update({code: filename})
         # If the file does not exist, create it and output the headers
         if(not os.path.exists(filename)):
@@ -105,51 +94,25 @@ def save_data(coinCodes, allData, current_datetime, file_upload_time, previous_f
                 np.savetxt(fname=outfile, X=numpy_data,
                            delimiter=',', fmt='%s')
 
-    # Upload the file to dropbox if the file_upload_time has been reached or if the filename changed.
-    # If the filename changed, then the final version of the previous file can be uploaded
-    # Note: This will overwrite any existing file on Dropbox
-    if(DEBUGGER == 1):
-        print("(current_datetime - file_upload_time).total_seconds() : " +
-              str((current_datetime - file_upload_time).total_seconds()))
-        print("(current_datetime - file_upload_time).total_seconds() >= 0 : " +
-              str((current_datetime - file_upload_time).total_seconds() >= 0))
-        print("previous_filenames : " + str(previous_filenames))
-        print("filenames : " + str(filenames))
-        print("previous_filenames != filenames : " +
-              str(previous_filenames != filenames))
-    if((current_datetime - file_upload_time).total_seconds() >= 0 or (previous_filenames != filenames)):
-        backup(coinCodes, previous_filenames, filenames)
-        previous_filenames = filenames
-        file_upload_time = 0
-
-    # Update the fileupload time, if an upload occurred in the previous executino
-    current_datetime = datetime.datetime.now(timezone('EST'))
-    if(file_upload_time == 0):
-        # delta = datetime.timedelta(hours=time_between_uploads)
-        delta = datetime.timedelta(minutes=time_between_uploads)
-        # delta = datetime.timedelta(seconds=time_between_uploads)
-        file_upload_time = current_datetime + delta
-    return {'filenames': filenames, 'file_upload_time': file_upload_time}
-
 
 def process_func(code):
     if(DEBUGGER == 1):
-        print("Starting process_func for code: " + str(code))
+        log("Starting process_func for code: " + str(code))
 
     try:
         # Get current quote from RobinHood
         local_dict = r.crypto.get_crypto_quote(code, info=None)
     except ConnectionError as err:
-        logging.error(
+        log(
             "Connection Error while retrieving a quote for " + code)
-        logging.error(err)
+        log(err)
     except Exception as err:
-        logging.error(
+        log(
             "Exception while retrieving a quote for " + code)
-        logging.error(err)
+        log(err)
 
     if(DEBUGGER == 1):
-        print("data fetched from robinhood " + str(local_dict))
+        log("data fetched from robinhood " + str(local_dict))
 
     # Append the datetime to the record
     est = timezone('EST')
@@ -159,67 +122,43 @@ def process_func(code):
     return local_dict
 
 
-# Uploads contents of LOCALFILE to Dropbox
-def backup(coinCodes, previous_filenames, filenames):
-    for code in coinCodes:
-        # On the first iteration of the program, the previous_filename will be null, and we want to update it to the new filname
-        previous_filename = None
-        if(previous_filenames is None):
-            previous_filename = filenames.get(code)
-        else:
-            previous_filename = previous_filenames.get(code)
-        filepath = previous_filename
-        backuppath = BACKUPPATH_ROOT + "/" + filepath
+def log(data):
+    # Create logging directory, if it doesn't exist
+    if(not os.path.exists(LOGGING_DIRECTORY)):
+        os.makedirs(LOGGING_DIRECTORY)
 
-        if(DEBUGGER == 1):
-            print('backuppath: ' + backuppath)
-            print('filepath: ' + filepath)
-            print("Creating a Dropbox object...")
+    # Create filename form current date
+    current_datetime = datetime.datetime.now(timezone('EST'))
+    filepath = LOGGING_DIRECTORY + "logs_" + \
+        current_datetime.strftime("%d_%m_%Y") + ".txt"
 
-        dbx = dropbox.Dropbox(TOKEN)
-
-        # Check that the access token is valid
-        try:
-            dbx.users_get_current_account()
-        except AuthError as err:
-            sys.exit(
-                "ERROR: Invalid access token; try re-generating an access token from the app console on the web.")
-
-        with open(filepath, 'rb') as f:
-            # We use WriteMode=overwrite to make sure that the settings in the file
-            # are changed on upload
-            if(DEBUGGER == 1):
-                print("Uploading " + filepath +
-                      " to Dropbox as " + backuppath + "...")
-
-            try:
-                dbx.files_upload(f.read(), backuppath,
-                                 mode=WriteMode('overwrite'))
-            except ApiError as err:
-                # This checks for the specific error where a user doesn't have enough Dropbox space quota to upload this file
-                if (err.error.is_path() and
-                        err.error.get_path().error.is_insufficient_space()):
-                    sys.exit("ERROR: Cannot back up; insufficient space.")
-                elif err.user_message_text:
-                    print(err.user_message_text)
-                    sys.exit()
-                else:
-                    print(err)
-                    sys.exit()
+    # Log data
+    with open(filepath, 'a') as f:
+        f.write(str(current_datetime) + "\t" + data)
+        f.write("\n-------------------------------------------------\n")
 
 
-# def logging(filename):
-#     filepath = l
-#     with open(filepath, 'rb') as f:
+def logCleanup():
+    log_files = [f for f in listdir(
+        LOGGING_DIRECTORY) if isfile(join(LOGGING_DIRECTORY, f))]
+    for log_file in log_files:
+        f_split = log_file.replace('.', '_')
+        f_split = f_split.split('_')
+        # f_split = re.split('_|.', local_file)
+        file_date_str = f_split[1] + "_" + f_split[2] + "_" + f_split[3]
+        file_date = datetime.datetime.strptime(
+            file_date_str, "%d_%m_%Y")
 
-#         # # Adding few functions to check file details
-#         # def checkFileDetails():
-#         #     print("Checking file details")
+        current_date_str = datetime.datetime.now(
+            timezone('EST')).strftime("%d_%m_%Y")
+        current_date = datetime.datetime.strptime(
+            current_date_str, "%d_%m_%Y")
 
-#         #     for entry in dbx.files_list_folder('').entries:
-#         #         print("File list is : ")
-#         #         print(entry.name)
-#         print("hello")
+        # If the file was created more than X days ago, delete it locally
+        if((current_date - file_date).days > 14):
+            filepath = LOGGING_DIRECTORY + log_file
+            os.remove(filepath)
+
 
 def readStatus():
     filename = LOCAL_DATA_DIRECTORY + "runData.json"
@@ -233,11 +172,9 @@ def readStatus():
             return json.load(infile)
 
 
-def saveStatus(file_upload_time, run_counter, previous_filenames):
+def saveStatus(run_counter):
     statusDict = {
-        "fileUploadTime": file_upload_time,
-        "run_counter": run_counter,
-        "previous_filenames": previous_filenames
+        "run_counter": run_counter
     }
     status = json.dumps(statusDict, indent=4, sort_keys=True, default=str)
 
@@ -262,20 +199,13 @@ def main():
     # This if statement should only occur on the first run
     if(status is None):
         statusDict = {
-            "fileUploadTime": datetime.datetime.now(timezone('EST')),
             "run_counter": 0,
-            "previous_filenames": None
         }
         status = json.dumps(
             statusDict, indent=4, sort_keys=True, default=str)
     status = json.loads(status)
 
-    file_upload_time = status["fileUploadTime"]
     run_counter = status["run_counter"]
-    previous_filenames = status["previous_filenames"]
-
-    # Convert file_upload_time from string to datetime
-    file_upload_time = datetime.datetime.fromisoformat(file_upload_time)
 
     # data - For each coin, save all data
     # previous_filename - For each coin, track when filename changes. This is needed so that if the filename changes between upload intervals, the final version of the previous file can be uploaded
@@ -284,16 +214,15 @@ def main():
         allData.update({code: []})
 
     if(DEBUGGER == 1):
-        print("coinCodes: " + str(coinCodes))
-        print("previous_filenames: " + str(previous_filenames))
-        print("allData: " + str(allData))
+        log("coinCodes: " + str(coinCodes))
+        log("allData: " + str(allData))
 
     current_datetime = datetime.datetime.now(timezone('EST'))
 
     # log each 5 loops
     if(run_counter % 5 == 0):
-        print("total_loop_counter: " + str(run_counter) +
-              ", datetime: " + str(current_datetime))
+        log("total_loop_counter: " + str(run_counter) +
+            ", datetime: " + str(current_datetime))
     run_counter += 1
 
     # For each coin, get its current market data
@@ -302,25 +231,23 @@ def main():
         coindata.append(process_func(code))
         allData.update({code: coindata})
         if(DEBUGGER == 1):
-            print("#################################################")
-            print("Inside coinCodes loop1")
-            print("Code: " + str(code))
-            print("allData: " + str(allData))
-            print("#################################################")
+            log("#################################################")
+            log("Inside coinCodes loop1")
+            log("Code: " + str(code))
+            log("allData: " + str(allData))
+            log("#################################################")
 
     if(DEBUGGER == 1):
-        print("#################################################")
-        print("Post coinCodes loop1")
-        print("allData: " + str(allData))
-        print("#################################################")
+        log("#################################################")
+        log("Post coinCodes loop1")
+        log("allData: " + str(allData))
+        log("#################################################")
 
     # Save the accumulated data
-    info_from_save_data = save_data(
-        coinCodes, allData, current_datetime, file_upload_time, previous_filenames)
-    previous_filenames = info_from_save_data.get("filenames")
-    file_upload_time = info_from_save_data.get("file_upload_time")
+    save_data(coinCodes, allData, current_datetime)
 
-    saveStatus(file_upload_time, run_counter, previous_filenames)
+    saveStatus(run_counter)
+    logCleanup()
 
 
 if __name__ == "__main__":
